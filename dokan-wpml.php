e9f22fea859433f5dff014a27169f80f7b23bd43
Alamgir <alamgircse.bd@gmail.com>
2020-12-03 15:40:20 +0600

    chore: version bump 1.0.3, changelog updated, generate pot files

--- dokan-wpml.php
+++ dokan-wpml.php
@@ -3,7 +3,7 @@
 Plugin Name: Dokan - WPML Integration
 Plugin URI: https://wedevs.com/
 Description: WPML and Dokan compitable package
-Version: 1.0.2
+Version: 1.0.3
 Author: weDevs
 Author URI: https://wedevs.com/
 Text Domain: dokan-wpml
