6865bf24099b03f3be92aebd6297a92340538191
Nurul Umbhiya <zikubd@gmail.com>
2023-12-12 19:07:07 +0600

    chore: bump version to 1.0.9

--- dokan-wpml.php
+++ dokan-wpml.php
@@ -3,12 +3,12 @@
  * Plugin Name: Dokan - WPML Integration
  * Plugin URI: https://wedevs.com/
  * Description: WPML and Dokan compatible package
- * Version: 1.0.8
+ * Version: 1.0.9
  * Author: weDevs
  * Author URI: https://wedevs.com/
  * Text Domain: dokan-wpml
  * WC requires at least: 5.5.0
- * WC tested up to: 7.7.2
+ * WC tested up to: 8.2.2
  * Domain Path: /languages/
  * License: GPL2
  */
