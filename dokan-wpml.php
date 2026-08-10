a6cd7e4f3f64798fb30f974079942bb9ed3e9985
Md Asif Hossain Nadim <devianadim@gmail.com>
2026-08-10 13:08:15 +0600

    chore: Release version 1.1.15

--- dokan-wpml.php
+++ dokan-wpml.php
@@ -3,7 +3,7 @@
  * Plugin Name: Dokan - WPML Integration
  * Plugin URI: https://wedevs.com/
  * Description: WPML and Dokan compatible package
- * Version: 1.1.14
+ * Version: 1.1.15
  * Author: weDevs
  * Author URI: https://wedevs.com/
  * Text Domain: dokan-wpml
